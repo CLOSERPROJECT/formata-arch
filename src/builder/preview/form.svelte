b18a461738f9ad732d8947a3bee6268d88c651e2
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-02-12 19:49:40 +0100

    feat: wire up component

--- src/builder/preview/form.svelte
+++ src/builder/preview/form.svelte
@@ -1,21 +1,20 @@
 <script lang="ts">
-  import { isRecordEmpty } from '@sjsf/form/lib/object';
+	import { isRecordEmpty } from '@sjsf/form/lib/object';
 
-  import { getBuilderContext } from "../context.svelte.js";
+	import { getBuilderContext } from '../context.svelte.js';
 
-  const ctx = getBuilderContext();
+	const ctx = getBuilderContext();
 
-  const schemaJson = $derived(JSON.stringify(ctx.schema));
-  const uiSchema = $derived(ctx.uiSchema ?? {});
-  const hasUiSchema = $derived(uiSchema && !isRecordEmpty(uiSchema));
-  const uiSchemaJson = $derived(JSON.stringify(uiSchema));
+	const schemaJson = $derived(ctx.schema);
+	const uiSchema = $derived(ctx.uiSchema ?? {});
+	const hasUiSchema = $derived(uiSchema && !isRecordEmpty(uiSchema));
+	const uiSchemaJson = $derived(uiSchema);
+	$inspect(schemaJson);
 </script>
 
 <div class="flex flex-col gap-2">
-  <div class="rounded-md border border-(--global-border) p-4">
-    <formata-form
-      schema={schemaJson}
-      ui-schema={hasUiSchema ? uiSchemaJson : undefined}
-    ></formata-form>
-  </div>
+	<div class="rounded-md border border-(--global-border) p-4">
+		<formata-form schema={schemaJson} ui-schema={hasUiSchema ? uiSchemaJson : undefined}
+		></formata-form>
+	</div>
 </div>
