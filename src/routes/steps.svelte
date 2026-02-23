8365bf30ae27adb876bd9111be48bfd7c806bc1c
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-02-23 11:48:08 +0100

    Delete steps.svelte

--- src/routes/steps.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Crud from '$core/crud.svelte';
-	import { StepRepository } from '$core/repositories/step.repository.js';
-	import { config } from '$core/state.svelte.js';
-
-	const repository = new StepRepository(config);
-</script>
-
-<Crud {repository} />
